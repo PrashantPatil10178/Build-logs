77ac23f55b08a9f6ce51173b4e11884e45794f0c
Prashant Patil <patilanilprashant178@gmail.com>
2025-05-12 22:32:39 +0530

    NewChanges

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Demo Node.js App!");
+  res.send("Hello from Demo Node.js App");
 });
 
 app.get("/health", (req, res) => {
